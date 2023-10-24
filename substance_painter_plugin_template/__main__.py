dc9069d60633c448df8b150b23433837a17e3c93
hannesdelbeke <hannesdelbeke@users.noreply.github.com>
2023-10-24 10:47:17 +0000

    ✅ Ready to clone and code.

---
+++ substance_painter_plugin_template/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for substance_painter_plugin_template."""
+
+from substance_painter_plugin_template.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- substance_painter_plugin_template/cli.py
+++ substance_painter_plugin_template/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for substance_painter_plugin_template project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m substance_painter_plugin_template` and `$ substance_painter_plugin_template `.
 
     This is your program's entry point.
 
